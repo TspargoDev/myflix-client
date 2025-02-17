7e19154ea7ddc4d09ef34e9f0ee88b8f04eb981f
Travis <Travisspargo28@gmail.com>
2025-02-16 22:32:24 -0500

    retry

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import React, { createRoot } from 'react-dom/client';
-import { MainView } from "./components/MainView/MainView.jsx";
+import { MainView } from "../components/MainView/MainView.jsx";
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
