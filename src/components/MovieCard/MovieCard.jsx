90964f54bc962c6358010b27db411d6f8e68b8e0
Travis <Travisspargo28@gmail.com>
2025-04-03 08:18:39 -0400

    test

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -3,29 +3,26 @@ import React from 'react';
 import { Button, Card } from 'react-bootstrap';
 
 export const MovieCard = ({ movie, onMovieClick }) => {
+  console.log(movie);
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img 
-        variant="top" 
-        src={movie.imageURL || 'https://via.placeholder.com/150'} // Fallback image URL if none provided
-        alt={movie.Title}
-      />
-      <Card.Body>
-        <Card.Title>{movie.Title}</Card.Title> {/* Capitalized 'Title' as per API response */}
-        <Card.Text>{movie.Description}</Card.Text> {/* Capitalized 'Description' */}
-        <Button variant="primary" onClick={() => onMovieClick(movie)}>View Details</Button>
-      </Card.Body>
-    </Card>
+      <Card className="h-100">
+          <Card.Img variant="top" src={movie.imagePath} className="w-100" />
+          <Card.Body>
+              <Card.Title>{movie.title}</Card.Title>
+              <Card.Text>{movie.director.name}</Card.Text>
+              <Button onClick={() => onMovieClick(movie)} variant="link">
+                  Open
+              </Button>
+          </Card.Body>
+      </Card>
   );
 };
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    Title: PropTypes.string.isRequired, // Ensure 'Title' is capitalized to match API response
-    Description: PropTypes.string.isRequired, // Ensure 'Description' is capitalized to match API response
-    imageURL: PropTypes.string,
+      title: PropTypes.string
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired,
+  onMovieClick: PropTypes.func.isRequired
 };
 
 export default MovieCard;
